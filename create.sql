6346b3eb3ae2c11faa3cabcd4231abf8e987964f
is2bwlla <lucyrose22198@gmail.com>
2025-06-03 16:56:37 -0300

    crud aprendiz

--- create.sql
+++ create.sql
@@ -140,3 +140,128 @@ create table tbinstrutores (id integer not null auto_increment, disciplina varch
         curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
         primary key (id)
     ) engine=InnoDB;
+
+    create table tbaprendizes (
+        ferias bit,
+        id integer not null auto_increment,
+        edv varchar(255),
+        email varchar(255),
+        nome varchar(255),
+        curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table tbinstrutores (
+        ativo bit,
+        ferias bit,
+        id integer not null auto_increment,
+        disciplina varchar(255),
+        edv varchar(255),
+        email varchar(255),
+        faculdade varchar(255),
+        nome varchar(255),
+        trilha varchar(255),
+        turma varchar(255),
+        curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table tbaprendizes (
+        ferias bit,
+        id integer not null auto_increment,
+        edv varchar(255),
+        email varchar(255),
+        nome varchar(255),
+        curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table tbinstrutores (
+        ativo bit,
+        ferias bit,
+        id integer not null auto_increment,
+        disciplina varchar(255),
+        edv varchar(255),
+        email varchar(255),
+        faculdade varchar(255),
+        nome varchar(255),
+        trilha varchar(255),
+        turma varchar(255),
+        curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table tbaprendizes (
+        ferias bit,
+        id integer not null auto_increment,
+        edv varchar(255),
+        email varchar(255),
+        nome varchar(255),
+        curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table tbinstrutores (
+        ativo bit,
+        ferias bit,
+        id integer not null auto_increment,
+        disciplina varchar(255),
+        edv varchar(255),
+        email varchar(255),
+        faculdade varchar(255),
+        nome varchar(255),
+        trilha varchar(255),
+        turma varchar(255),
+        curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table tbaprendizes (
+        ferias bit,
+        id integer not null auto_increment,
+        edv varchar(255),
+        email varchar(255),
+        nome varchar(255),
+        curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table tbinstrutores (
+        ativo bit,
+        ferias bit,
+        id integer not null auto_increment,
+        disciplina varchar(255),
+        edv varchar(255),
+        email varchar(255),
+        faculdade varchar(255),
+        nome varchar(255),
+        trilha varchar(255),
+        turma varchar(255),
+        curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table tbaprendizes (
+        ferias bit,
+        id integer not null auto_increment,
+        edv varchar(255),
+        email varchar(255),
+        nome varchar(255),
+        curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table tbinstrutores (
+        ativo bit,
+        ferias bit,
+        id integer not null auto_increment,
+        disciplina varchar(255),
+        edv varchar(255),
+        email varchar(255),
+        faculdade varchar(255),
+        nome varchar(255),
+        trilha varchar(255),
+        turma varchar(255),
+        curso enum ('ADM','DS','MANUFATURA','MECATRONICA'),
+        primary key (id)
+    ) engine=InnoDB;
--- src/main/java/br/ETS/Feedback/controller/AprendizController.java
+++ src/main/java/br/ETS/Feedback/controller/AprendizController.java
@@ -6,10 +6,15 @@ import br.ETS.Feedback.model.instrutor.DadosAtualizacaoInstrutor;
 import br.ETS.Feedback.model.instrutor.DadosListagemInstrutor;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping ("/aprendiz")
@@ -26,6 +31,12 @@ public class AprendizController {
         return ResponseEntity.created(uri).body(new DadosListagemAprendiz(aprendiz));
     }
 
+    @GetMapping
+    public ResponseEntity<List<DadosListagemAprendiz>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var aprendiz = repository.findAll(paginacao).map(DadosListagemAprendiz::new);
+        return ResponseEntity.ok(aprendiz.getContent());
+    }
+
     @GetMapping("/{id}")
     @Transactional
     public ResponseEntity<DadosInformacoesCompletasDoAprendiz> detalharAprendiz(@PathVariable int id) {
@@ -34,10 +45,17 @@ public class AprendizController {
     }
 
     @Transactional
-    @PutMapping
+    @PutMapping("/{id}")
     public ResponseEntity<DadosListagemAprendiz> atualizar(@RequestBody @Valid DadosAtualizacaoAprendiz dadosAtualizacaoAprendiz) {
         var aprendiz = repository.getReferenceById(dadosAtualizacaoAprendiz.id());
         aprendiz.atualizar(dadosAtualizacaoAprendiz);
         return ResponseEntity.ok(new DadosListagemAprendiz(aprendiz));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Void> excluir(@PathVariable int id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
